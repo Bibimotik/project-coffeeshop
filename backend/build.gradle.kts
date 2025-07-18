b8b9a0201b22817f49f60b5f6d3d2a622496b4c1
Mikhail <misha2005.b@yandex.ru>
2025-07-18 11:49:52 +0300

    feat: add mapstruct for mapping

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 }
 
 tasks.withType<Test> {
