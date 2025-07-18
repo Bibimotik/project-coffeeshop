ad695000be7a565f8309d504c00c48f4998a76ec
Mikhail <misha2005.b@yandex.ru>
2025-07-18 11:46:54 +0300

    feat: add methods for clients

--- backend/src/main/java/org/backend/model/clients/ClientsRepository.java
+++ backend/src/main/java/org/backend/model/clients/ClientsRepository.java
@@ -1,11 +1,17 @@
 package org.backend.model.clients;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientsRepository extends JpaRepository<Clients, UUID> {
   Optional<Clients> findById(UUID id);
+  boolean existsById(UUID id);
+  boolean existsByEmail(String email);
+  boolean existsByPhone(String phone);
+  void deleteById(UUID id);
+  default boolean existsClientsByEmailOrPhone(String email, String phone) {
+    return existsByEmail(email) || existsByPhone(phone);
+  };
 }
