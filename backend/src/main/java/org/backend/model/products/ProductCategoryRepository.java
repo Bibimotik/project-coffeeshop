166ef448d1849e432059e6a35db782eafa45d1c6
Mikhail <misha2005.b@yandex.ru>
2025-08-04 17:47:35 +0300

    feat: add repository for categories

---
+++ backend/src/main/java/org/backend/model/products/ProductCategoryRepository.java
@@ -0,0 +1,6 @@
+package org.backend.model.products;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+}
